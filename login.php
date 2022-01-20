0e7b3da6341791fc43bc01170ef81d5bab46cd43
nitronukes <89461502+nitronukes@users.noreply.github.com>
2022-01-20 13:31:39 +0100

    Update login.php

--- login.php
+++ login.php
@@ -10,20 +10,19 @@
     <title>Document</title>
 </head>
 <body>
-
 <fieldset class="container">
     <legend style='margin-left:45%;'>Login</legend>
     <form method="POST" action="#">
     <div class="user">
     <i class="fas fa-user"></i>
-    <input class="geb-naam" type="txt" name="gebruikersnaam"placeholder="Gebruikersnaam">
+    <input class="geb-naam" type="txt" id="txt_uname" name="txt_uname"placeholder="Gebruikersnaam">
     <br>
    </div>
    <div class="ww">
    <i class="fas fa-key"></i>
-    <input class="wachtw" type="password" name="wachtwoord" placeholder="Wachtwoord"><br>
+    <input class="wachtw" type="password" id="txt_uname"  name="txt_pwd" placeholder="Wachtwoord"><br>
 </div>
-    <button class="login"type="submit" name="submit">Login</button><br>
+    <button class="login" value="Submit" type="submit" name="but_submit" id="but_submit" name="but_submit">Login</button><br>
 </form>
 <center>
 <p>als je geen account hebt klik <a class="a" href="#">hier</a><p>
@@ -33,3 +32,36 @@
 
 </body>
 </html>
+
+
+
+<?php
+
+include "config.php";
+
+
+if(isset($_POST['but_submit'])){
+
+    $uname = mysqli_real_escape_string($con,$_POST['txt_uname']);
+    $password = mysqli_real_escape_string($con,$_POST['txt_pwd']);
+
+
+    if ($uname != "" && $password != ""){
+
+        $sql_query = "select count(*) as cntUser from users where username='".$uname."' and password='".$password."'";
+        $result = mysqli_query($con,$sql_query);
+        $row = mysqli_fetch_array($result);
+
+        $count = $row['cntUser'];
+
+        if($count > 0){
+            $_SESSION['uname'] = $uname;
+            header('Location: uitloggen.php');
+        }else{
+            echo "Invalid username and password";
+        }
+
+    }
+
+}
+?>
