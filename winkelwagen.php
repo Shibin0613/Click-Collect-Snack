0468df5e82d91fa16f18ef92e5621716b5bdf872
Jesper <jesperminks7@gmail.com>
2022-01-25 09:20:29 +0100

    puuuuuuuuuuush

---
+++ winkelwagen.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="winkelwagen.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <title>Winkelwagen</title>
+</head>
+
+<body>
+        <div class="winkelwagenmain">
+
+             <br>
+            <div class="product">
+                <img width="60" height="50" src="http://www.snackweetjes.nl/wp-content/uploads/2016/03/frituur-snacks.png" alt="productfoto">
+                <p class="producttitlecart" href="www.frikandel.nl">Frikandel</p>
+            </div>
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            
+        </div>
+</body>
+
+</html>
