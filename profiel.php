7332abe0b65c6a6047f6e67b2027767bb16eac71
MartijnGraafsma <91067234+MartijnGraafsma@users.noreply.github.com>
2022-01-26 09:43:17 +0100

    tyh

---
+++ profiel.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="profiel.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    <title>profiel</title>
+</head>
+<body>
+    <center>
+    <div class="profiel-container">
+        <h2 class="profiel">Profiel bewerken</h2>
+        <form method="POST" action="#">
+            <p>naam:</p>
+            <input class="naam" type="txt" name="naam" placeholder="Naam"><br>
+            <input class="naam" type="txt" name="email" placeholder="E-mail"><br>
+            <input class="naam" type="txt" name="telefoon" placeholder="Telefoon"><br>
+            <button class="submit" type="submit" name="submit">Opslaan</button>
+        </form> 
+    </div>
+</center>
+</body>
+</html>
