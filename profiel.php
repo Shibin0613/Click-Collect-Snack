a9f9cb7a140b63cc0412e369d1fa5bc947cacec2
Shibin0613 <220617@edu.rocfriesepoort.nl>
2022-02-08 10:45:39 +0100

    Update profiel.php

--- profiel.php
+++ profiel.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/profiel1.css">
+    <link rel="stylesheet" href="css/profiel.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <title>profiel</title>
@@ -13,10 +13,10 @@
     <center>
     <div class="profiel-container">
         <h2 class="profiel">Snackie toevoegen</h2>
-        <form method="POST" action="#">
-            <input class="naam" type="txt" name="naam" placeholder="Naam"><br>
-            <input class="naam" type="txt" name="bedrag" placeholder="Bedrag"><br>
-            <input class="" type="file" id="file" name="image" accept="image/*" placeholder="Voeg foto toe">
+        <form method="POST" action="" enctype="multipart/form-data">
+            <input class="naam" type="txt" name="productnaam" placeholder="Naam" required><br>
+            <input class="naam" type="txt" name="bedrag" placeholder="Bedrag"required><br>
+            <input class="" type="file" id="file" name="image" accept="image/*" required>
                 <label for="file">
                   <i class="far fa-file-image"></i> &nbsp;
                   Voeg foto toe  
@@ -30,28 +30,24 @@
         </div>
     </div>
 </center>
-
-
-
 </body>
 </html>
 
 <?php
 include "conn.php";
-if(isset($_POST['submit'])){
-   $naam= $_POST['naam'];
-   $bedrag = $_POST['bedrag'];
-    $sql="INSERT INTO `producten`(`naam`, `bedrag`) values(?,?)";
-    $stmt= $conn->prepare($sql);
-    $stmt->bind_param("ss", $naam, $bedrag);
-    $stmt->execute();
+
+if(isset($_POST['submit'])) {
+    $productnaam=$_POST['productnaam'];
+    $bedrag=$_POST['bedrag'];
+    $foto = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));
+    
+    $query = "INSERT INTO product(`productnaam`,`bedrag`,`foto`) VALUES('$productnaam','$bedrag','$foto') ";
+    $query_run= mysqli_query($conn, $query);
+    
+    if($query_run){
+        echo "<script>alert('Product is toegevoegd.')</script>";
+    }else{
+        echo "<script>alert('Het is niet gelukt')</script>";
+    }
 }
 ?>
-
-<!-- zorgt dat het niet opnieuw toegevoegd wordt aan de database als je refreshed -->
-<script>
-if ( window.history.replaceState ) {
-  window.history.replaceState( null, null, window.location.href );
-}
-</script>
-
