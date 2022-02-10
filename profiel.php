93a50291f05b44d5068b7ac921052b9880e5ca46
Shibin0613 <220617@edu.rocfriesepoort.nl>
2022-02-10 13:41:59 +0100

    Create profiel.php

---
+++ profiel.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="profiel.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <title>profiel</title>
+</head>
+<body>
+    <center>
+    <div class="profiel-container">
+        <h2 class="profiel">Snackie toevoegen</h2>
+        <form method="POST" action="" enctype="multipart/form-data">
+            <input class="naam" type="txt" name="productnaam" placeholder="Naam" required><br>
+            <input class="naam" type="txt" name="bedrag" placeholder="Bedrag"required><br>
+            <input class="" type="file" id="file" name="image" accept="image/*" required>
+                <label for="file">
+                  <i class="far fa-file-image"></i> &nbsp;
+                  Voeg foto toe  
+                </label>
+            <button class="submit" type="submit" name="submit">Opslaan</button>
+
+        </form> 
+        <!-- moet nog gemaakt worden -->
+        <div class="foto-container">
+            <h3 class="foto">foto</h3>
+        </div>
+    </div>
+</center>
+</body>
+</html>
+
+<?php
+include "conn.php";
+
+if(isset($_POST['submit'])) {
+    $productnaam=$_POST['productnaam'];
+    $bedrag=$_POST['bedrag'];
+    
+    $image = $_FILES['image']['name'];
+    $target = "image/".basename($image);
+    
+    $query = "INSERT INTO product(`productnaam`,`bedrag`,`foto`) VALUES('$productnaam','$bedrag','$image') ";
+    $query_run= mysqli_query($conn, $query);
+
+    if(move_uploaded_file($_FILES['image']['tmp_name'], $target)){
+    
+    if($query_run){
+        echo "<script>alert('Product is toegevoegd.')</script>";
+    }else{
+        echo "<script>alert('Het is niet gelukt')</script>";
+    }
+}
+}
+?>
