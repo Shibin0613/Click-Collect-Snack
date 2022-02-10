c2f65bc22dee1e5569e41e59600ff058e50dc382
Jesper <jesperminks7@gmail.com>
2022-02-10 14:10:50 +0100

    ooga booga

--- winkelwagen.php
+++ winkelwagen.php
@@ -5,11 +5,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/winkelwagen1.css">
+    <link rel="stylesheet" href="css/winkelwagen.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <title>Winkelwagen</title>
 </head>
+
+<?php
+    $GLOBALS["Products"] = [
+        0 => [
+            "Name" => "Frikandel",
+            "Amount" => 1,
+            "PricePerUnitEur" => 1.50,
+            "Image" => "http://www.snackweetjes.nl/wp-content/uploads/2016/03/frituur-snacks.png"
+        ],
+
+        1 => [
+            "Name" => "Kroket",
+            "Amount" => 2,
+            "PricePerUnitEur" => 2,
+            "Image" => "http://www.snackweetjes.nl/wp-content/uploads/2016/03/frituur-snacks.png"
+        ],
+
+        2 => [
+            "Name" => "Kromartijnket",
+            "Amount" => 2,
+            "PricePerUnitEur" => 25,
+            "Image" => "http://www.snackweetjes.nl/wp-content/uploads/2016/03/frituur-snacks.png"
+        ],
+    ];
+?>
 
 <body>
     <div class="flex">
@@ -33,39 +58,18 @@
 
 
             <?php
-            $Products = [
-                0 => [
-                    "Name" => "Frikandel",
-                    "Amount" => 1,
-                    "PricePerUnitEur" => 1.50,
-                    "Image" => "http://www.snackweetjes.nl/wp-content/uploads/2016/03/frituur-snacks.png"
-                ],
-
-                1 => [
-                    "Name" => "Kroket",
-                    "Amount" => 3,
-                    "PricePerUnitEur" => 2,
-                    "Image" => "http://www.snackweetjes.nl/wp-content/uploads/2016/03/frituur-snacks.png"
-                ],
-
-                2 => [
-                    "Name" => "Kromartijnket",
-                    "Amount" => 2,
-                    "PricePerUnitEur" => 25,
-                    "Image" => "http://www.snackweetjes.nl/wp-content/uploads/2016/03/frituur-snacks.png"
-                ],
-            ];
+            $Products = $GLOBALS["Products"];
+            $TotalPrice = 0;
 
             foreach ($Products as $Key => $Value) {
-                //$V = $Value["Name"];
-                //echo "{$Key} => {$V} ";
-
                 $Name = $Value["Name"];
                 $Amount = $Value["Amount"];
                 $PPU = $Value["PricePerUnitEur"];
                 $Img = $Value["Image"];
 
                 $TotalProductPrice = $Amount * $PPU;
+
+                $TotalPrice = $TotalPrice + $TotalProductPrice;
 
                 echo "
                     <br>
@@ -76,12 +80,15 @@
                             <p class='producttitlecart'>$Name</p>
                         </div>
 
-                            <button class='productminuscart'>-</button>
-                            <p class='productamountcart'>x$Amount</p>
-                            <button class='productpluscart'>+</button>
-                            <p class='pricecart'>€$TotalProductPrice</p>
+                            <form class='freeform' method='POST' action='' enctype='multipart/form-data'>
+                                <p class='productamountcart' name='productamount'>x$Amount</p>
+                                <p class='pricecart'>€$TotalProductPrice</p>
+                            </form>
                     </div>";
             }
+
+            
+            $GLOBALS["TotalProductPrice"] = $TotalPrice;
             ?>
         </div>
 
@@ -102,7 +109,13 @@
                     <p>Totaal:</p>
                 </div>
                 <div class="alignright">
-                    <p>€3,00</p>
+                    <?php
+                        $TotalePrijs = number_format( $GLOBALS["TotalProductPrice"], 2, "," );
+
+                        echo "
+                            <p>€$TotalePrijs</p>
+                        ";
+                    ?>
                 </div>
             </div>
 
