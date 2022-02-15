548d988a3e326905d73ca7c8af1198f537e3d0ab
Shibin0613 <220617@edu.rocfriesepoort.nl>
2022-02-15 10:43:51 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,12 +7,11 @@ if(isset($_POST['bestel'])){
     $email=$_POST['email'];
     $ophaaltijd=$_POST['ophaaltijd'];
     
-    $receiver = $email;
     $subject= "U heeft een bestelling geplaatst";
     $body = "Beste $achternaam, \r\nu heeft een bestelling gedaan
     \r\nU kunt uw bestelling komen ophalen om $ophaaltijd. \r\n\r\nEet smakelijk!\r\n\r\nMet vriendelijke groet,\r\nSnackhoek";
     
-    if(mail($receiver, $subject, $body)){
+    if(mail($email, $subject, $body)){
         echo "<script>alert('Het is gelukt om een bestelling te plaatsen!')</script>";
         ?>
         <META HTTP-EQUIV="Refresh" CONTENT="0; URL=bestelpagina.php">
