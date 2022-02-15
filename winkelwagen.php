ccb0abc569c9d1ca325d0ca6109a870d6bd5684a
Shibin0613 <220617@edu.rocfriesepoort.nl>
2022-02-15 10:43:55 +0100

    Update winkelwagen.php

--- winkelwagen.php
+++ winkelwagen.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+// checken als de klant niet heeft ingelogd
+
+// logout
+if(isset($_POST['loguit'])){
+    session_destroy();
+    header('Location: login.php');
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,45 +16,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/winkelwagen.css">
+    <link rel="stylesheet" href="css/winkelwagen1.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <title>Winkelwagen</title>
 </head>
 
-<?php
-    $GLOBALS["Products"] = [
-        0 => [
-            "Name" => "Frikandel",
-            "Amount" => 1,
-            "PricePerUnitEur" => 1.50,
-            "Image" => "http://www.snackweetjes.nl/wp-content/uploads/2016/03/frituur-snacks.png"
-        ],
-
-        1 => [
-            "Name" => "Kroket",
-            "Amount" => 2,
-            "PricePerUnitEur" => 2,
-            "Image" => "http://www.snackweetjes.nl/wp-content/uploads/2016/03/frituur-snacks.png"
-        ],
-
-        2 => [
-            "Name" => "Kromartijnket",
-            "Amount" => 2,
-            "PricePerUnitEur" => 25,
-            "Image" => "http://www.snackweetjes.nl/wp-content/uploads/2016/03/frituur-snacks.png"
-        ],
-    ];
-?>
-
 <body>
     <div class="flex">
         <div class="titlediv">
             <h1 class="titletext">Winkelwagen</h1>
-            <?php
-                $ProductAmount = 1;
-                echo "<p class='titleamount'>$ProductAmount producten</p>"
-            ?>
         </div>
 
         <div class="titledivsummary">
@@ -55,40 +37,32 @@
 
     <div class="flex">
         <div class="winkelwagenmain">
-
-
+            
             <?php
-            $Products = $GLOBALS["Products"];
-            $TotalPrice = 0;
+            if(isset($_SESSION['wagen'])){
+                foreach ($_SESSION['wagen'] as $key => $product) {
+                //$V = $Value["Name"];
+                //echo "{$Key} => {$V} ";
 
-            foreach ($Products as $Key => $Value) {
-                $Name = $Value["Name"];
-                $Amount = $Value["Amount"];
-                $PPU = $Value["PricePerUnitEur"];
-                $Img = $Value["Image"];
+                $productnaam = $product['productnaam'];
+                $aantal = $product['aantal'];
+                $bedrag = $product['bedrag'];
 
-                $TotalProductPrice = $Amount * $PPU;
-
-                $TotalPrice = $TotalPrice + $TotalProductPrice;
+                $TotalProductPrice = $aantal * $bedrag;
 
                 echo "
                     <br>
                     <div class='product'>
-                        <img width='60' height='50' src='$Img' alt='productfoto'>
-                    
                         <div class='producttitlediv'>
-                            <p class='producttitlecart'>$Name</p>
+                            <p class='producttitlecart'>$productnaam</p>
                         </div>
-
-                            <form class='freeform' method='POST' action='' enctype='multipart/form-data'>
-                                <p class='productamountcart' name='productamount'>x$Amount</p>
-                                <p class='pricecart'>€$TotalProductPrice</p>
-                            </form>
+                            <button class='productminuscart'>-</button>
+                            <p class='productamountcart'>x$aantal</p>
+                            <button class='productpluscart'>+</button>
+                            <p class='pricecart'>€$TotalProductPrice</p>
                     </div>";
+                }
             }
-
-            
-            $GLOBALS["TotalProductPrice"] = $TotalPrice;
             ?>
         </div>
 
@@ -109,19 +83,16 @@
                     <p>Totaal:</p>
                 </div>
                 <div class="alignright">
-                    <?php
-                        $TotalePrijs = number_format( $GLOBALS["TotalProductPrice"], 2, "," );
-
-                        echo "
-                            <p>€$TotalePrijs</p>
-                        ";
-                    ?>
+                    <p>€3,00</p>
                 </div>
             </div>
 
             <button class="orderbtn">Bestel nu</button>
         </div>
     </div>
+    <form method='POST' action="" >
+        <input type="submit" value="Logout" name="loguit">
+    </form>
 </body>
 
 </html>
