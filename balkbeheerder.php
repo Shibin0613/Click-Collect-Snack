17e1db635a7176f2e662a4d841fed4eb837cecb9
Shibin0613 <220617@edu.rocfriesepoort.nl>
2022-02-25 15:48:55 +0100

    Update balkbeheerder.php

--- balkbeheerder.php
+++ balkbeheerder.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title> 
+    <title>Beheer</title> 
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/balk1.css">
 </head>
@@ -13,6 +13,9 @@
         <nav class="topnav">
                 <a href="beheer.php">Beheer</a>
                 <a href="snackietoevoegen.php">Snackie toevoegen</a>
+                <form method='POST' action="" >
+                    <button type="submit" name="loguit">Loguit</button>
+                </form>
                 
         </nav>
     </div>
