46614505453b9027b65c422f5bd3957c81d36398
Shibin0613 <220617@edu.rocfriesepoort.nl>
2022-02-25 15:49:00 +0100

    Update balkklant.php

--- balkklant.php
+++ balkklant.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title> 
+    <title>Klant</title> 
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/balk1.css">
 </head>
@@ -14,6 +14,17 @@
                 
                 <a href="bestelpagina.php">Bestel</a>
                 <a href="winkelwagen.php">Winkelwagen</a>
+                <?php
+                if(isset($_SESSION['wagen'])){
+                    $count = count($_SESSION['wagen']);
+                    echo "<span>$count</span>";
+                }else{
+                    echo "<span>0</span>";
+                }
+                ?>
+                <form method='POST' action="" >
+                    <button type="submit" name="loguit">Loguit</button>
+                </form>
                 
         </nav>
     </div>
